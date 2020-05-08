b2e23566a69ad3d279f49af49b7fc18722e00823
rnfnal73 <40377057+rnfnal73@users.noreply.github.com>
2020-05-08 23:13:03 +0900

    COMMIT

---
+++ test_repo/src/main/java/Test_package/Test.java
@@ -0,0 +1,6 @@
+package Test_package;
+
+public class Test {
+    //asdwdzxcs
+    hello;
+}
---
+++ test_repo/src/test/java/Test_package/TestTest.java
@@ -0,0 +1,7 @@
+package Test_package;
+
+import junit.framework.TestCase;
+
+public class TestTest extends TestCase {
+    
+}
